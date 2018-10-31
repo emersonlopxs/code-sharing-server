a333451ea7a29f229a3790c53320397d4a289427
lupuselit <lupuselit@outlook.com>
2018-10-31 09:24:09 -0300

    new files

--- server/app.js
+++ server/app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.json({
+    message: 'Hello world!'
+  });
 });
+
+app.post('/', (req, res) => {});
 
 app.listen(3000, () => console.log('server is running on port 3000'));
