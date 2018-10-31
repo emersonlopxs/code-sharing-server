13a1a2d27e4f9ef76c0b565eddff4a2bfe371a38
lupuselit <lupuselit@outlook.com>
2018-10-31 10:57:57 -0300

    build react

---
+++ client/build/precache-manifest.0498301b150c5a9b3b6b80cd1e1082f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3ff34c4513d4c672c91f",
+    "url": "/static/js/main.3ff34c45.chunk.js"
+  },
+  {
+    "revision": "ed5f83da0a52e50a698c",
+    "url": "/static/js/1.ed5f83da.chunk.js"
+  },
+  {
+    "revision": "3ff34c4513d4c672c91f",
+    "url": "/static/css/main.01babff5.chunk.css"
+  },
+  {
+    "revision": "994ab583dca121190cd745e839783c3e",
+    "url": "/index.html"
+  }
+];
