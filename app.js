a0455ed50ad84a47f3bf447c6cad69e333bd0053
lupuselit <lupuselit@outlook.com>
2018-12-17 22:26:53 -0200

    add port

--- app.js
+++ app.js
@@ -54,4 +54,4 @@ app.post('/api', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('server is running on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('server is running on port 3000'));
